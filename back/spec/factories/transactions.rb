0f5c442d796b86a2681533371db7b55b9d40a824
jinxin4869 <jinxin10244869@gmail.com>
2026-02-16 21:51:24 +0900

    TransactionsController の RSpec テストを追加
    認証済みCRUD操作、期間・タイプフィルタリング、サマリー計算、
    未認証アクセス拒否、他ユーザーの取引へのアクセス制限をテスト。
    transactions ファクトリーも新規作成。

---
+++ back/spec/factories/transactions.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :transaction do
+    association :user
+    amount { 1000 }
+    transaction_type { :expense }
+    description { "テスト取引" }
+    category { "食費" }
+    date { Date.current }
+  end
+end
